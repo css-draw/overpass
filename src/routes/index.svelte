f56441ef62e664678587e842104e7b2b491697c9
shynome <shynome@gmail.com>
2022-01-01 22:55:47 +0800

    ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="box">
+	<div class="overpass1">
+		<div class="border" />
+	</div>
+	<div class="overpass2">
+		<div class="border" />
+	</div>
+</div>
+
+<style>
+	@import './index.css';
+</style>
